d1db018e1d159336979fc66fd154baabb53e1596
juliano.barbosa <juliano.barbosa@lambda3.com.br>
2022-03-18 16:51:31 -0300

    add ssl links doc to future configs

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const basePath = process.env.NODE_ENV === "development" ? "src" : "dist";
 const NODE_ENV = process.env.NODE_ENV;
 const ssl = NODE_ENV === "production" ? true : false;
-
+/**
+ * When activating ssl, use
+ * https://devcenter.heroku.com/articles/ssl-certificate-self
+ * https://devcenter.heroku.com/articles/ssl-endpoint#setting-up-ssl-on-heroku
+ */
 module.exports = {
   type: "postgres",
   url: `${process.env.DATABASE_URL}`,
